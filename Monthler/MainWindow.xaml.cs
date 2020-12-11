e82483c31d59c9a42268d9f433d20bd5350234b0
Chadfire <chadfire10@gmail.com>
2020-12-11 14:04:35 -0700

    Pin and unpin window

--- Monthler/MainWindow.xaml.cs
+++ Monthler/MainWindow.xaml.cs
@@ -32,5 +32,34 @@ namespace Monthler
         {
             InitializeComponent();
         }
+
+        #region Pinning window
+
+        /// <summary>
+        /// Pins the calendar window above all other windows.
+        /// </summary>
+        private void BtnPinWindow_Click(object sender, RoutedEventArgs e)
+        {
+            isWindowPinned = !isWindowPinned;
+            this.Topmost = isWindowPinned;
+            this.ChangePinText();
+        }
+
+        /// <summary>
+        /// Changes the pin's btn text, based on if the window is pinned or not.
+        /// </summary>
+        private void ChangePinText()
+        {
+            if (isWindowPinned)
+            {
+                BtnPinWindow.Content = "Unpin Window";
+            }
+            else
+            {
+                BtnPinWindow.Content = "Pin Window";
+            }
+        }
+
+        #endregion
     }
 }
